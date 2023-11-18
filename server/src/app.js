4e816caf57fa8ea214a398d6a31b7a0736390e7c
Rishu <rrishu561@gmail.com>
2023-11-19 00:13:13 +0530

    fix: build details

--- server/src/app.js
+++ server/src/app.js
@@ -22,13 +22,15 @@ app.use(express.urlencoded({ extended: true, limit: `${LIMIT}` }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.use(express.static(path.resolve(__dirname, "bleedink", "dist")));
+
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/posts", postRoute);
 app.use("/api/v1/dashboard", dashboardRoute);
 app.use("/api/v1/gpt", gptRoute);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "bleedink", "dist", "index.html"));
 });
 
 export default app;
