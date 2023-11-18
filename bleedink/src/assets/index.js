4261126b3b3a58e5f924abcfc7c91f1741a1500e
Rishu <rrishu561@gmail.com>
2023-11-18 16:18:16 +0530

    fix: spell error

--- bleedink/src/assets/index.js
+++ bleedink/src/assets/index.js
@@ -1,5 +1,5 @@
 export { default as loaderAnimation } from "./loaderAnimation.json";
-export { default as logo } from "./Logo.jpg";
+export { default as logo } from "./logo.jpg";
 export { default as userAvatar } from "./userAvatar.png";
 export { default as defCover } from "./defCover.jpg";
 export { default as postBackground } from "./postBackground.jpg";
